061164c2f4646597c0863ccc2109ea161e7b2be4
Furukawa <135786069+naoyuki2@users.noreply.github.com>
2025-04-14 01:21:47 +0000

    GETをPOSTに変更

--- public/try1/login.php
+++ public/try1/login.php
@@ -8,12 +8,12 @@
   <body class="cyberpunk-bg">
     <div class="login-box">
 	<?php
-		if($_GET['username']=="asosys" &&
-		    $_GET['password']=="abcc2291")
-		{
-			echo "<h2>ログイン成功だ！ようこそHACKの世界へ</h2>";
-		}else{
-			echo "<h2>ログイン失敗だ！もう一度確認しろ</h2>";
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			if ($_POST['username'] == "asosys" && $_POST['password'] == "abcc2291") {
+				echo "<h2>ログイン成功だ！ようこそHACKの世界へ</h2>";
+			} else {
+				echo "<h2>ログイン失敗だ！もう一度確認しろ</h2>";
+			}
 		}
 	?>
     </div>
